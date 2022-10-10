702f093d57eebbb2bf8a9c3997f75d36564c426e
AtikAkbar <atik.akbar02@gmail.com>
2022-10-10 19:09:08 +0600

    configered the enviornment to use tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// use this file just to use the tailwind css extention
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
